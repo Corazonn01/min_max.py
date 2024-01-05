4546d780fee5eb7a51cad6990c9f84e7a5680716
Elina Makipova <82317623+Corazonn01@users.noreply.github.com>
2024-01-05 19:15:48 +0300

    min_max.py

---
+++ min_max.py
@@ -0,0 +1,23 @@
+def bubble_sort(array):
+    length = len(array)
+    for i in range(0, length):
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+
+
+arr = []
+n = int(input("Напишите несколько цифр: "))
+for i in range(0, n):
+    element = int(input("arr[" + str(i + 1) + "] = "))
+    arr.append(element)
+bubble_sort(arr)
+print("Отсортированный массив: ")
+average = sum(arr) / len(arr)
+print(arr)
+print(min(arr))
+print(max(arr))
+print(average)
+
